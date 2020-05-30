76d86e9e9c19c396a33703a14a4320ed990fc776
Ankur Rajput <ankur.rajput@argildx.com>
2020-05-31 02:27:54 +0530

    4 - added Google provider for google OAuth2

--- core/src/main/java/com/tgt/core/aem/social/login/constants/SocialLoginConstants.java
+++ core/src/main/java/com/tgt/core/aem/social/login/constants/SocialLoginConstants.java
@@ -8,6 +8,8 @@ public final class SocialLoginConstants {
     private SocialLoginConstants() {
     }
 
+    public static final String THE_GREY_TEACHER = "thegreyteacher";
+
     /**
      * {@value org.apache.oltu.oauth2.common.OAuth#OAUTH_GRANT_TYPE} to get access token from social
      * provider.
@@ -36,5 +38,13 @@ public final class SocialLoginConstants {
      * Google URL to fetch user details using access token
      */
     public static final String GOOGLE_USER_DETAILS_URL = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json";
+
+    /**
+     * Google providers property which will be used to create user ID in AEM.
+     * <p>
+     * In our case we will use user's email as AEM user ID so this will be the property in user details data fetched
+     * from Google having user's email ID as value.
+     */
+    public static final String GOOGLE_PROVIDER_USER_ID_PROPERTY_NAME = "email";
 
 }
